7a4fd86cb8faee8f7c18019212818a79d0da2524
Franco Pizzi <fran.pizzi@hotmail.com>
2021-12-13 19:16:26 -0300

    HW 13/12

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,8 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+
+  
 }
 
 
@@ -14,6 +16,7 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+   
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,13 +24,14 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+  
 }
 
 function eliminarPropiedad(objeto, unaPropiedad) {
@@ -35,12 +39,14 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  
 }
 
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+  
 
 }
 
@@ -48,14 +54,26 @@ function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if (usuario.email === null || usuario.email === undefined) {
+    return false
+  }
+  return true;
+  
 }
 
-
+// hasta aca van pasados 
 function tienePropiedad(objeto, propiedad) {
   // Devuelve "true" si el objeto tiene el valor del argumento "propiedad"
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (objeto.hasOwnProperty(propiedad)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+
 }
 
 function verificarPassword(usuario, password) {
@@ -63,12 +81,22 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (usuario.password === password) {
+    return true;
+  }
+  else {
+    return false;
+  }
+  
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevaPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword;
+  return usuario;
+  
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -76,6 +104,9 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // Tu código:
+  usuario.amigos.push(nuevoAmigo);
+  return usuario;
+  
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -84,6 +115,11 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for (var i = 0; i < usuarios.length ; i++) {
+    usuarios[i].esPremium = true;
+  }
+  return usuarios;
+  
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -93,6 +129,16 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var suma = 0;
+  for (let i = 0 ; i < usuario.posts.length ; i++) {
+    suma = suma + usuario.posts[i].likes;
+    //aca solo se puede acceder a i
+  }
+
+  //aca no voy a poder acceder a i
+  return suma;
+
+  
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -105,6 +151,12 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  producto.calcularPrecioDescuento = function () {
+    var resultado = this.precio - (this.precio * this.porcentajeDeDescuento)
+    return resultado;
+  }
+  return producto; 
+
 
 }
 
